d700ec084fe8ee494bb448ceb3a61871074e5767
Mahit Mehta <mahit.py@gmail.com>
2023-06-08 23:36:25 -0400

    Added Dockerfiles

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+# Using the `rust-musl-builder` as base image, instead of 
+# the official Rust toolchain
+FROM clux/muslrust:stable AS chef
+USER root
+RUN cargo install cargo-chef
+WORKDIR /app
+
+FROM chef AS planner
+COPY . .
+RUN cargo chef prepare --recipe-path recipe.json
+
+FROM chef AS builder
+COPY --from=planner /app/recipe.json recipe.json
+# Notice that we are specifying the --target flag!
+RUN cargo chef cook --release --target x86_64-unknown-linux-musl --recipe-path recipe.json
+COPY . .
+RUN cargo build --release --target x86_64-unknown-linux-musl --bin mahitm_vpn_server
+
+FROM alpine AS runtime
+RUN addgroup -S myuser && adduser -S myuser -G myuser
+RUN apk add -U --no-cache wireguard-tools
+COPY --from=builder /app/target/x86_64-unknown-linux-musl/release/mahitm_vpn_server /usr/local/bin/
+USER myuser
+CMD ["/usr/local/bin/mahitm_vpn_server"]
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder };
+use std::process::Command; 
+use log::info; 
+use env_logger::Env;
 
 #[get("/")]
 async fn hello() -> impl Responder {
+    info!("Pinged Root: /");
     HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/create-file")]
+async fn api() -> impl Responder {
+    Command::new("touch")
+        .arg("file.txt")
+        .output()
+        .expect("failed to execute process");
+
+    HttpResponse::Ok().body("Created File")
 }
 
 #[post("/echo")]
@@ -16,13 +30,16 @@ async fn manual_hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     HttpServer::new(|| {
         App::new()
             .service(hello)
             .service(echo)
+            .service(api)
             .route("/hey", web::get().to(manual_hello))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
